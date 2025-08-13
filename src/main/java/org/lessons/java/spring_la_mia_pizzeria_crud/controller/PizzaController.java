cf131a953905b8d42975ee5050900c9645f24948
martinoalanza03 <lanzamartino3@gmail.com>
2025-08-13 11:04:59 +0200

    feat: add list of pizze to PizzaController

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -1,16 +1,25 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 
+import java.util.List;
+import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 
 @Controller
 @RequestMapping("/pizze")
 public class PizzaController {
 
+    @Autowired
+    private PizzaRepository repository;
+
     @GetMapping
     public String index(Model model) {
+        List<Pizza> pizze = repository.findAll();
+        model.addAttribute("pizze", pizze);
         return "pizze/index";
     }
 
