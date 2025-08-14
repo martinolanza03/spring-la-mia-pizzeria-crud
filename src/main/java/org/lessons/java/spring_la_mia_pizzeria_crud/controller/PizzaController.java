3df179b575d9de007cbda1db0b220d2d57c84125
martinoalanza03 <lanzamartino3@gmail.com>
2025-08-14 12:02:35 +0200

    feat: method post to create for save pizza

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -5,10 +5,17 @@ import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/pizze")
@@ -36,6 +43,20 @@ public class PizzaController {
         model.addAttribute("pizza", new Pizza());
 
         return "pizze/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "pizze/create";
+        }
+
+        repository.save(formPizza);
+
+        return "redirect:/pizze";
     }
 
 }
