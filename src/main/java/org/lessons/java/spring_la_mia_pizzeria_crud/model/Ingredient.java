ff6e0505d6f379ccc8ef0059605dcd62b20459c6
martinoalanza03 <lanzamartino3@gmail.com>
2025-08-28 11:38:51 +0200

    feat: create class ingridient

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Ingredient.java
@@ -0,0 +1,65 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "ingridients")
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "The name must not be null, nor empty or blank")
+    private String name;
+
+    private String descritpion;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(Integer id, String name, String descritpion) {
+        this.id = id;
+        this.name = name;
+        this.descritpion = descritpion;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescritpion() {
+        return this.descritpion;
+    }
+
+    public void setDescritpion(String descritpion) {
+        this.descritpion = descritpion;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", name='" + getName() + "'" +
+                ", descritpion='" + getDescritpion() + "'" +
+                "}";
+    }
+
+}
