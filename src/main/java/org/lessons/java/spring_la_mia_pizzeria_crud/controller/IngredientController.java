81a6bb551489ee5aff0c2a8295e95bb74f6b2eaf
martinoalanza03 <lanzamartino3@gmail.com>
2025-08-28 12:16:26 +0200

    feat: implement show for ingredient

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,5 +24,11 @@ public class IngredientController {
         List<Ingredient> ingredients = repository.findAll();
         model.addAttribute("ingredients", ingredients);
         return "ingredient/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("ingredient", repository.findById(id).get());
+        return "ingredient/show";
     }
 }
