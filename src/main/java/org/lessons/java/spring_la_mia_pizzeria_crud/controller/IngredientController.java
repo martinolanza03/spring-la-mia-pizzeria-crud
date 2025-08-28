319739b8d93bb44d8e36dc3b80dc79f7311f1792
martinoalanza03 <lanzamartino3@gmail.com>
2025-08-28 12:11:29 +0200

    feat: implement index for ingredient

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
@@ -1,8 +1,14 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 
+import java.util.List;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Ingredient;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.IngridientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +17,11 @@ public class IngredientController {
 
     @Autowired
     private IngridientRepository repository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Ingredient> ingredients = repository.findAll();
+        model.addAttribute("ingredients", ingredients);
+        return "ingredient/index";
+    }
 }
